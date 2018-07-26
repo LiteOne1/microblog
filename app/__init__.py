f3c3a3d0dc93aba2510276f8cd3c62ae482c7015
LiteOne1 <39678787+LiteOne1@users.noreply.github.com>
2018-07-26 17:25:09 +0300

    26.07 Database
    until migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, url_for
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)#import configuration from config.py
-
+db = SQLAlchemy(app)#database related variables
+migrate = Migrate(app, db)
 
 from app import routes
-
